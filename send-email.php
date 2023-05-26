f833da17de9b46fe4f1bd248c9b93b3343b4c8ec
Akhil Mahesh <124284817+Akhil-Mahesh@users.noreply.github.com>
2023-05-26 16:34:51 +0530

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,17 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
-  $to = "akhilmahesh012@gmail.com";
-  $subject = "New message from $name";
-  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-  $headers = "From: $email";
-
-  if (mail($to, $subject, $body, $headers)) {
-    echo "Message sent!";
-  } else {
-    echo "Error sending message.";
-  }
-}
-?>
+// AlonePhilic 
